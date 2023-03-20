367ccd8f3b6f05a94c3050ea9aa3136750d8d708
sonnysideupp <sonnyhuang2@gmail.com>
2023-03-19 23:14:22 -0400

    reformat

--- src/routes/movies/+page.svelte
+++ src/routes/movies/+page.svelte
@@ -1,11 +1,11 @@
-
-
 <script>
+// @ts-nocheck
+
 	import { onMount } from 'svelte';
-	import MovieCard from "./MovieCard.svelte"
-	import Filter from "./Filter.svelte"
-	import MovieCardLoader from "./MovieCardLoader.svelte"
-	import { browser } from '$app/environment'; 
+	import MovieCard from './MovieCard.svelte';
+	import Filter from './Filter.svelte';
+	import MovieCardLoader from './MovieCardLoader.svelte';
+	import { browser } from '$app/environment';
 	import { searchQuery, sortBy, selectedGenres } from '../stores.js';
 	import { onDestroy } from 'svelte';
 
@@ -13,27 +13,24 @@
 	let ogMovies = [];
 
 	// movies shown to user
-	let movies = []
+	let movies = [];
 
 	// store the new batch of data here.
 	let newBatch = [];
 
 	let timer;
 
+	let endPoint = 'https://api.themoviedb.org/3/discover/movie?';
 
-	let endPoint = "https://api.themoviedb.org/3/discover/movie?"
+	let currentPage = 1;
 
-	let currentPage = 1
+	let totalResults = 0;
 
-	let totalResults = 0
+	let totalPages = 0;
 
-	let totalPages = 0
+	let isFetching = true;
 
-	let isFetching = true
-
-
-	let isSearch = false
-
+	let isSearch = false;
 
 	let searchParams = new URLSearchParams({
 		api_key: '7edefdf731502b6398dd9b4d2c622614',
@@ -41,307 +38,223 @@
 		primary_release_year: 1995,
 		sort_by: $sortBy,
 		page: 1
-	})
+	});
 
-	$: movies = [
-		...movies,
-    ...newBatch
-  ];
+	$: movies = [...movies, ...newBatch];
 
+	$: ogMovies = [...ogMovies, ...newBatch];
 
-  $: ogMovies = [
-	...ogMovies,
-	...newBatch
-  ]
-
-	$: if($searchQuery != ""){
+	$: if ($searchQuery != '') {
 		if (browser) {
-
-		
-
-		// debounce for searching with a query
-		clearTimeout(timer);
-		timer = setTimeout(() => {
-		isSearch = true
-		searchMovie($searchQuery)
-		}, 500);
-		
-		
-
+			// debounce for searching with a query
+			clearTimeout(timer);
+			timer = setTimeout(() => {
+				isSearch = true;
+				searchMovie($searchQuery);
+			}, 500);
 		}
-	}else{
+	} else {
 		// resetting search to discover
-		
+
 		setTimeout(() => {
-		isSearch = false
-		endPoint = "https://api.themoviedb.org/3/discover/movie?"
-		fetchMovies("popularity.desc", [])
-
-		}, 500)
-
-
+			isSearch = false;
+			endPoint = 'https://api.themoviedb.org/3/discover/movie?';
+			fetchMovies('popularity.desc', []);
+		}, 500);
 	}
 
-	$:     // make it react to changes (in the parent)
-	if (browser) {
-
-	if (isSearch){
-
-		// filtering current movies if movies are from user search
-		sortMovies($sortBy)
-
-	}else{
-
-		fetchMovies()
+	// make it react to changes (in the parent)
+	$: if (browser) {
+		if (isSearch) {
+			// filtering current movies if movies are from user search
+			sortMovies($sortBy);
+		} else {
+			fetchMovies();
+		}
 	}
-	}
-   ;
-
-   $: if ($selectedGenres.length >= 0){
-	if (browser) {
-
-	if (isSearch){
-		// filtering current movies if movies are from user search
-		filterMoviesInPlace($selectedGenres)
-		
-	}
-	else{
-
-		fetchMovies()
+	$: if ($selectedGenres.length >= 0) {
+		if (browser) {
+			if (isSearch) {
+				// filtering current movies if movies are from user search
+				filterMoviesInPlace($selectedGenres);
+			} else {
+				fetchMovies();
+			}
 		}
 	}
 
-   }
-
-   // sorting movies by field
-   const sortMovies = (sort_option) => {
-
-
-		switch(sort_option){
-			case "popularity.desc": 
-				movies =  movies.sort((a,b) => b.popularity - a.popularity) 
-				break
-			case "popularity.asc": 
-				movies =  movies.sort((a,b) => a.popularity - b.popularity) 
-				break
-			case "vote_average.asc": 
-				movies =  movies.sort((a,b) => a.vote_average - b.vote_average) 
-				break
-			case "vote_average.desc": 
-				movies =  movies.sort((a,b) => b.vote_average - a.vote_average) 
-				break
-			case "primary_release_date.asc": 
-				movies =  movies.sort((a,b) => new Date(a.release_date) - new Date(b.release_date))  
-				break
-			case "primary_release_date.desc": 
-				movies =  movies.sort((a,b) => new Date(b.release_date) - new Date(a.release_date)) 
-				break
-
-
-
+	// sorting movies by field
+	const sortMovies = (sort_option) => {
+		switch (sort_option) {
+			case 'popularity.desc':
+				movies = movies.sort((a, b) => b.popularity - a.popularity);
+				break;
+			case 'popularity.asc':
+				movies = movies.sort((a, b) => a.popularity - b.popularity);
+				break;
+			case 'vote_average.asc':
+				movies = movies.sort((a, b) => a.vote_average - b.vote_average);
+				break;
+			case 'vote_average.desc':
+				movies = movies.sort((a, b) => b.vote_average - a.vote_average);
+				break;
+			case 'primary_release_date.asc':
+				movies = movies.sort((a, b) => new Date(a.release_date) - new Date(b.release_date));
+				break;
+			case 'primary_release_date.desc':
+				movies = movies.sort((a, b) => new Date(b.release_date) - new Date(a.release_date));
+				break;
 		}
+	};
 
-   }
-
-
-   // filtering and sorting movies by genres in place
-   const filterMoviesInPlace = (generes) => {
-
-		if(generes.length > 0){
-			movies = ogMovies.filter(el => generes.every(r=> el.genre_ids.includes(r)))
-			
-		}else{
+	// filtering and sorting movies by genres in place
+	const filterMoviesInPlace = (generes) => {
+		if (generes.length > 0) {
+			movies = ogMovies.filter((el) => generes.every((r) => el.genre_ids.includes(r)));
+		} else {
 			// resetting movies to original movies
-			movies = ogMovies
-			
+			movies = ogMovies;
 		}
-		sortMovies($sortBy)
-   }
+		sortMovies($sortBy);
+	};
 
-   // filtering movies by genres 
-   const filterMovies = (array, generes) => {
-
-		if(generes.length > 0){
-			return array.filter(el => generes.every(r=> el.genre_ids.includes(r)))
-			
-		}else{
-			
-			return array
-		
+	// filtering movies by genres
+	const filterMovies = (array, generes) => {
+		if (generes.length > 0) {
+			return array.filter((el) => generes.every((r) => el.genre_ids.includes(r)));
+		} else {
+			return array;
 		}
+	};
 
-   }
+	const searchMovie = async (query) => {
+		try {
+			movies = [];
+			isFetching = true;
+			currentPage = 1;
+			endPoint = 'https://api.themoviedb.org/3/search/movie?';
 
-   const searchMovie = async (query) => {
+			searchParams = new URLSearchParams({
+				api_key: '7edefdf731502b6398dd9b4d2c622614',
+				include_adult: false,
+				query: query,
+				page: 1
+			});
+			const res = await fetch(endPoint + searchParams);
+			let response = await res.json();
 
-	try{
-	movies = []
-	isFetching = true
-	currentPage = 1
-	endPoint = "https://api.themoviedb.org/3/search/movie?"
-
-	searchParams = new URLSearchParams({
-		api_key: '7edefdf731502b6398dd9b4d2c622614',
-		include_adult: false,
-		query: query,
-		page: 1
-	})
-	const res = await fetch(
-		endPoint + searchParams
-	);
-	let response = await res.json();
-
-
-	isFetching = false
-	ogMovies = response.results
-	movies = filterMovies(response.results, $selectedGenres)
-	sortMovies($sortBy)
-	totalResults = response.total_results
-	totalPages = response.total_pages
-
-
-	
-	}catch(error){
-		isFetching = false
-		console.log("fetch movies error")
-		console.log(error)
-	}
-
-   }
-
-   const fetchMoreMovies = async (params, page) => {
-
-	try{
-
-	if (page > totalPages) return 
-
-	params.set("page", page)
-	
-	
-	isFetching = true
-	const res = await fetch(
-		endPoint + params
-	);
-	let response = await res.json();
-
-	
-	totalResults = response.total_results
-	totalPages = response.total_pages
-	if(response.results){
-
-		newBatch = filterMovies(response.results, $selectedGenres)
-	
-	}
-
-	}catch(error){
-		
-		console.log("fetching more movies error")
-		console.log(error)
-	}
-
-   }
-
-  
-
-   const onScroll = async (e) => {
-
-	if (!isFetching && (window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-
-		if ($selectedGenres.length > 0) {
-			searchParams.set("with_genres", $selectedGenres.join(","))
+			isFetching = false;
+			ogMovies = response.results;
+			movies = filterMovies(response.results, $selectedGenres);
+			sortMovies($sortBy);
+			totalResults = response.total_results;
+			totalPages = response.total_pages;
+		} catch (error) {
+			isFetching = false;
+			console.log('fetch movies error');
+			console.log(error);
 		}
-		await fetchMoreMovies(searchParams, ++currentPage)
-		isFetching = false
-  }
-   }
+	};
 
+	const fetchMoreMovies = async (params, page) => {
+		try {
+			if (page > totalPages) return;
 
-   const fetchMovies = async () => {
+			params.set('page', page);
 
-	try{
+			isFetching = true;
+			const res = await fetch(endPoint + params);
+			let response = await res.json();
 
-	let sort = $sortBy
-	let genres = $selectedGenres
-	if (isSearch) return
-	movies = []
-	
-	// resetting search params to original
-	searchParams = new URLSearchParams({
-		api_key: '7edefdf731502b6398dd9b4d2c622614',
-		include_adult: false,
-		primary_release_year: 1995,
-		sort_by: sort,
-		page: 1
-	})
-	if(genres.length > 0){
+			totalResults = response.total_results;
+			totalPages = response.total_pages;
+			if (response.results) {
+				newBatch = filterMovies(response.results, $selectedGenres);
+			}
+		} catch (error) {
+			console.log('fetching more movies error');
+			console.log(error);
+		}
+	};
 
-	searchParams.set("with_genres", genres.join(","))
+	const onScroll = async (e) => {
+		if (!isFetching && window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+			if ($selectedGenres.length > 0) {
+				searchParams.set('with_genres', $selectedGenres.join(','));
+			}
+			await fetchMoreMovies(searchParams, ++currentPage);
+			isFetching = false;
+		}
+	};
 
-	}
+	const fetchMovies = async () => {
+		try {
+			let sort = $sortBy;
+			let genres = $selectedGenres;
+			if (isSearch) return;
+			movies = [];
 
-	endPoint = "https://api.themoviedb.org/3/discover/movie?"
-	isFetching = true
-	currentPage = 1
-	const res = await fetch(
-		endPoint + searchParams
-	);
-	let response = await res.json();
+			// resetting search params to original
+			searchParams = new URLSearchParams({
+				api_key: '7edefdf731502b6398dd9b4d2c622614',
+				include_adult: false,
+				primary_release_year: 1995,
+				sort_by: sort,
+				page: 1
+			});
+			if (genres.length > 0) {
+				searchParams.set('with_genres', genres.join(','));
+			}
 
+			endPoint = 'https://api.themoviedb.org/3/discover/movie?';
+			isFetching = true;
+			currentPage = 1;
+			const res = await fetch(endPoint + searchParams);
+			let response = await res.json();
 
-	
-	isFetching = false
-	movies = response.results
-	ogMovies = response.results
-	totalResults = response.total_results
-	totalPages = response.total_pages
+			isFetching = false;
+			movies = response.results;
+			ogMovies = response.results;
+			totalResults = response.total_results;
+			totalPages = response.total_pages;
+		} catch (error) {
+			isFetching = false;
+			console.log('fetch movies error');
+			console.log(error);
+		}
+	};
 
-	
-	}catch(error){
-		isFetching = false
-		console.log("fetch movies error")
-		console.log(error)
-	}
-   }
-
-
-	onMount(() => fetchMovies("popularity.desc", []));
-
+	onMount(() => fetchMovies('popularity.desc', []));
 </script>
 
 <svelte:window on:scroll={onScroll} />
 
+<div class="flex lg:flex-row  mt-24 flex-col">
+	<Filter />
 
-<div class="flex lg:flex-row  mt-24 flex-col"> 
-
-<Filter />
-
-<div class="flex flex-col w-full lg:w-4/5" > 
-
-	<div class="flex flex-row justify-end w-full mb-0 md:mb-2"> 
-
-		<div class="mr-8"> 
-			{totalResults} {totalResults > 1 ? "movies" : "movie"}
-		</div> 
-	
-	</div> 
-<div class="grid md:grid-cols-3 lg:grid-cols-4 grid-cols-2 gap-6 mb-8">
-	{#each movies as movie, index (`${movie.id}${index}`)}
-		<MovieCard movie={movie}/>
-	{:else}
-		<!-- this block renders when photos.length === 0 -->
-
-		{#if isFetching}
-
-			{#each [1,2,3,4,5,6,7,8] as loader, index (index) }
-			<MovieCardLoader/>
-			{/each}
-		{:else}
-			<div class="flex flex-col items-center w-full ml-4 lg:ml-0"> 
-			<p class="text-gray-400 text-2xl"> No movies found </p>
+	<div class="flex flex-col w-full lg:w-4/5">
+		<div class="flex flex-row justify-end w-full mb-0 md:mb-2">
+			<div class="mr-8">
+				{totalResults}
+				{totalResults > 1 ? 'movies' : 'movie'}
 			</div>
-		{/if}
-	{/each}
-</div>
+		</div>
+		<div class="grid md:grid-cols-3 lg:grid-cols-4 grid-cols-2 gap-6 mb-8">
+			{#each movies as movie, index (`${movie.id}${index}`)}
+				<MovieCard {movie} />
+			{:else}
+				<!-- this block renders when photos.length === 0 -->
 
-</div>
+				{#if isFetching}
+					{#each [1, 2, 3, 4, 5, 6, 7, 8] as loader, index (index)}
+						<MovieCardLoader />
+					{/each}
+				{:else}
+					<div class="flex flex-col items-center w-full ml-4 lg:ml-0">
+						<p class="text-gray-400 text-2xl">No movies found</p>
+					</div>
+				{/if}
+			{/each}
+		</div>
+	</div>
 </div>
--- src/routes/movies/Filter.svelte
+++ src/routes/movies/Filter.svelte
@@ -1,88 +1,92 @@
 <script>
+	// @ts-nocheck
+
 	import { sortBy, selectedGenres } from '../stores.js';
-	import {DropdownDivider, Chevron, Dropdown, DropdownItem, } from 'flowbite-svelte'
-	import sortByDic from "../../util/sortByDic.js"
+	import { DropdownDivider, Chevron, Dropdown, DropdownItem } from 'flowbite-svelte';
+	import sortByDic from '../../util/sortByDic.js';
 
-	const genres = [{id: 28, name: "Action"} , {id: 12, name: "Adventure"}, {id: 16, name: "Animation"}, {id: 35, name: "Comedy"},
-{id: 80, name: "Crime"}, {id: 99, name: "Documentary"}, {id: 18, name: "Drama"}, {id: 14, name: "Fantasy"}, {id: 36, name: "History"}, {id: 27, name: "Horror"}
-, {id: 9648, name: "Mystery"}, {id: 10749, name: "Romance"}, {id: 878, name: "Science Fiction"}, {id: 53, name: "Thriller"}, {id: 10752, name: "War"}, {id: 37, name: "Western"}]
-	
-let dropdownOpen = false;
+	const genres = [
+		{ id: 28, name: 'Action' },
+		{ id: 12, name: 'Adventure' },
+		{ id: 16, name: 'Animation' },
+		{ id: 35, name: 'Comedy' },
+		{ id: 80, name: 'Crime' },
+		{ id: 99, name: 'Documentary' },
+		{ id: 18, name: 'Drama' },
+		{ id: 14, name: 'Fantasy' },
+		{ id: 36, name: 'History' },
+		{ id: 27, name: 'Horror' },
+		{ id: 9648, name: 'Mystery' },
+		{ id: 10749, name: 'Romance' },
+		{ id: 878, name: 'Science Fiction' },
+		{ id: 53, name: 'Thriller' },
+		{ id: 10752, name: 'War' },
+		{ id: 37, name: 'Western' }
+	];
 
-const selectedGenre = (genre) => {
-	if ($selectedGenres.includes(genre.id)){
+	let dropdownOpen = false;
 
-	
-		selectedGenres.set($selectedGenres.filter(el => el != genre.id))
-		
-	}else{
-
-
-		selectedGenres.set([...$selectedGenres, genre.id])
-
-		
-	}
-
-}
-
+	const selectedGenre = (genre) => {
+		if ($selectedGenres.includes(genre.id)) {
+			selectedGenres.set($selectedGenres.filter((el) => el != genre.id));
+		} else {
+			selectedGenres.set([...$selectedGenres, genre.id]);
+		}
+	};
 </script>
 
+<div class="flex flex-col items-start md:mt-8 mt-0 mb-2 lg:mb-0 w-full lg:w-auto">
+	<button
+		class="flex flex-row items-center justify-between rounded-xl lg:w-80  md:w-[calc(100%-4rem)] md:mx-8  w-[calc(100%-2rem)]  mx-4 drop-shadow-2xl bg-white"
+	>
+		<div class="px-4 py-3 text-lg font-semibold">Sort by</div>
 
-<div class="flex flex-col items-start md:mt-8 mt-0 mb-2 lg:mb-0 w-full lg:w-auto"> 
+		<div class="flex flex-row items-center mr-4">
+			<div class="text-md font-sans">
+				{sortByDic[$sortBy]}
+			</div>
 
-	<button class="flex flex-row items-center justify-between rounded-xl lg:w-80  md:w-[calc(100%-4rem)] md:mx-8  w-[calc(100%-2rem)]  mx-4 drop-shadow-2xl bg-white"> 
-
-		<div class="px-4 py-3 text-lg font-semibold">
-		Sort by
+			<Chevron />
 		</div>
-
-		<div class="flex flex-row items-center mr-4"> 
-		<div class="text-md font-sans"> 
-		{sortByDic[$sortBy]}
-		</div> 
-
-		<Chevron/>
-		</div> 
-
-		
-
-	
-
 	</button>
-	<Dropdown class="lg:w-80  md:w-[calc(100%-4rem)] w-[calc(100%-2rem)]  rounded-xl overflow-hidden" bind:open={dropdownOpen}>
-		{#each Object.keys(sortByDic) as sortParam  }
-		<button class="text-md font-sans p-2 w-full text-left bg-white hover:bg-gray-100" on:click={() => {
-			dropdownOpen = false
-			sortBy.set(sortParam) }}>{sortByDic[sortParam]}</button>
+	<Dropdown
+		class="lg:w-80  md:w-[calc(100%-4rem)] w-[calc(100%-2rem)]  rounded-xl overflow-hidden"
+		bind:open={dropdownOpen}
+	>
+		{#each Object.keys(sortByDic) as sortParam}
+			<button
+				class="text-md font-sans p-2 w-full text-left bg-white hover:bg-gray-100"
+				on:click={() => {
+					dropdownOpen = false;
+					sortBy.set(sortParam);
+				}}>{sortByDic[sortParam]}</button
+			>
+		{/each}
+	</Dropdown>
 
-	{/each}
-	  </Dropdown>
+	<div
+		class="rounded-xl lg:w-80  md:w-[calc(100%-4rem)] md:mx-8  w-[calc(100%-2rem)]  mx-4  drop-shadow-2xl bg-white mt-6"
+	>
+		<div class="px-4 pt-4 text-lg font-semibold">Filter</div>
+		<DropdownDivider />
 
+		<div class="px-4 text-gray-500 my-2">Genres</div>
 
-<div class="rounded-xl lg:w-80  md:w-[calc(100%-4rem)] md:mx-8  w-[calc(100%-2rem)]  mx-4  drop-shadow-2xl bg-white mt-6"> 
-
-<div class="px-4 pt-4 text-lg font-semibold">
-Filter
-</div>
-<DropdownDivider/>
-
-<div class="px-4 text-gray-500 my-2"> 
-Genres
-</div>
-
-<div class="flex flex-row flex-wrap px-4 mb-4"> 
-
-	{#each genres as genre (genre.id)}
-		<button on:click = {() => {selectedGenre(genre)}} class= {`px-3 my-1.5 mr-1.5 border hover:border-blue-700 rounded-3xl ${$selectedGenres.includes(genre.id) ? " bg-blue-700 text-white border-blue-700" : " bg-white text-black  border-gray-300"} hover:bg-blue-700 hover:text-white`}> 
-			{genre.name}
-		</button>
-
-	{/each}
-
-</div>
-
-</div> 
-
-
-
+		<div class="flex flex-row flex-wrap px-4 mb-4">
+			{#each genres as genre (genre.id)}
+				<button
+					on:click={() => {
+						selectedGenre(genre);
+					}}
+					class={`px-3 my-1.5 mr-1.5 border hover:border-blue-700 rounded-3xl ${
+						$selectedGenres.includes(genre.id)
+							? ' bg-blue-700 text-white border-blue-700'
+							: ' bg-white text-black  border-gray-300'
+					} hover:bg-blue-700 hover:text-white`}
+				>
+					{genre.name}
+				</button>
+			{/each}
+		</div>
+	</div>
 </div>
